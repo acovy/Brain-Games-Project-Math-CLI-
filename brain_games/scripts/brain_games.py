ed48a4e796f99e432bb78eb89d2e4e79e8252e80
Andrey Semin <aseminmma@gmail.com>
2022-12-10 13:56:07 +0300

    update all

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python3
+from brain_games.cli import welcome_user
 
+#def main():
+#    print('Welcome to the Brain Games!')
+#welcome_user()
+#
+#if name == 'main':
+#   main()
 def main():
-    print("Welcome to the Brain Games!")
-
-
+    print('Welcome to the Brain Games!')
+    welcome_user()
+ 
 if __name__ == '__main__':
     main()
