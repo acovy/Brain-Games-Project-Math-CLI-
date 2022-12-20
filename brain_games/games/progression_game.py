83ecbcb2662ca5041f802998c73d1f0260fd1d02
Andrey Semin <aseminmma@gmail.com>
2022-12-20 22:19:03 +0300

    fix brain progression

--- brain_games/games/progression_game.py
+++ brain_games/games/progression_game.py
@@ -3,13 +3,14 @@ from random import randint
 
 DESCRIPTION = 'What number is missing in the progression?'
 
+
 def generate_question_answer():
-    start_digit = randint(1, 15)
-    count_digits = randint(5, 10)
-    progression = [(i * start_digit) for i in range(1, 11)]
-    correct_answer = progression[count_digits]
-    progression[count_digits] = '..'
-    progression = [str(k) for k in progression]
+    num = randint(2, 6)
+    i_replace = randint(2, 9)
+    progression = [(i * num) for i in range(1, 11)]
+    correct_answer = progression[i_replace]
+    progression[i_replace] = '..'
+    progression = [str(a) for a in progression]
     progression = " ".join(progression)
     question = f'Question: {progression}' + "\n"
     return question, str(correct_answer)
