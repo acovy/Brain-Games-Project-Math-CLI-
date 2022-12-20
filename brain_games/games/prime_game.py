a353ebd3aaaab06c3751c3589d74fb6368973f57
Andrey Semin <aseminmma@gmail.com>
2022-12-20 22:22:53 +0300

    fix brain prime

--- brain_games/games/prime_game.py
+++ brain_games/games/prime_game.py
@@ -1,26 +1,28 @@
 from random import randint
 
-
 LOWER_LIMIT = 1
 UPPER_LIMIT = 100
 
-DESCRIPTION = 'What number is missing in the progression?'
+
+DESCRIPTION = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
-def prime(random_number):
+def is_prime(random_number):
     for i in range(2, random_number):
         if random_number % i == 0:
-            prime = False
+            is_prime = False
             break
     else:
-        prime = True
+        is_prime = True
     if random_number == 1:
-        prime = False
-    return prime
+        is_prime = False
+    return is_prime
 
 
 def generate_question_answer():
+    """This function defines the question for the game."""
     random_number = randint(LOWER_LIMIT, UPPER_LIMIT)
     question = f'Question: {random_number}' + "\n"
-    correct_answer = "yes" if prime(random_number) is True else "no"
+    correct_answer = "yes" if is_prime(random_number) is True else "no"
     return question, correct_answer
+    
